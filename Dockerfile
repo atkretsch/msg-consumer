92960defc154be61a34cdcc55a5ab46ae0be1e88
Andy Kretschmar <akretschmar@geninfo.com>
2018-08-07 13:32:25 -0500

    initial consumer implementation

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM microsoft/dotnet:sdk AS build-env
+WORKDIR /app
+
+# Copy csproj and restore as distinct layers
+COPY *.csproj ./
+RUN dotnet restore
+
+# Copy everything else and build
+COPY . ./
+RUN dotnet publish -c Release -o out
+
+# Build runtime image
+FROM microsoft/dotnet:runtime
+WORKDIR /app
+COPY --from=build-env /app/out .
+ENV QUEUE_URL=https://sqs.us-east-1.amazonaws.com/103029031786/test
+ENTRYPOINT ["dotnet", "MsgConsumer.dll"]
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace MsgConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var queueUrl = Environment.GetEnvironmentVariable("QUEUE_URL");
+            var client = new AmazonSQSClient();
+            while (true)
+            {
+                var receiveRequest = new ReceiveMessageRequest(queueUrl){
+                    WaitTimeSeconds = 20,
+                    AttributeNames = new List<string> { "All" },
+                    MessageAttributeNames = new List<string> { "All" }
+                };
+                var response = client.ReceiveMessageAsync(receiveRequest).Result;
+                var received = DateTime.UtcNow;
+                Console.WriteLine($"Received {response.Messages.Count} messages");
+                foreach (var message in response.Messages)
+                {
+                    Console.WriteLine(message.MessageAttributes.Count);
+                    var sentAttr = message.MessageAttributes["Sent"];
+                    var sent = DateTime.Parse(sentAttr.StringValue, null, DateTimeStyles.RoundtripKind);
+
+                    var sysSentAttr = message.Attributes["SentTimestamp"];
+                    var sysSent = DateTime.UnixEpoch.AddMilliseconds(double.Parse(sysSentAttr));
+
+                    Console.WriteLine($"Received message: {message.Body} (sent={sent:o}, sysSent={sysSent:o}, queueTime={received-sent})");
+                    client.DeleteMessageAsync(queueUrl, message.ReceiptHandle).Wait();
+                }
+            }
+        }
+    }
+}
